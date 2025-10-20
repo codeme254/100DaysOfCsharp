47aea04edf16c4912a4bc7c66899d3f9843f9ae7
codeme254 <otwomadennis365@gmail.com>
2025-10-20 11:23:10 +0300

    Day 1
    - Basic structure of a C# Program
    - Console Class methods and properties

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Title = "C# is Awesome";
+            Console.WriteLine(Console.Title); // C# is Awesome
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(Console.BackgroundColor);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(Console.ForegroundColor);
+
+
+            Console.Write("Hello, World");
+            Console.WriteLine("Hello, World");
+
+            Console.Write("Type a character: ");
+            int value = Console.Read();
+            Console.WriteLine("Ascii is " + value);
+
+            Console.Write("What is your full name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            Console.Write("Press Any key: ");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine(key);
+        }
+        
+    }
+}
