bf3702857379ad059575ce901099f0f82654fde7
codeme254 <otwomadennis365@gmail.com>
2025-10-27 06:47:59 +0300

    Day 8
    - Checked and unchecked keywords
    - Stack and Heap memory
    - Boxing and Unboxing in C#

---
+++ Day8/Day8/BoxingAndUnboxing.cs
@@ -0,0 +1,13 @@
+﻿namespace BoxingAndUnboxing
+{
+    internal class BoxingAndUnboxing
+    {
+        static void Main()
+        {
+            int x = 10;
+            object y = x; // boxing
+            int z = (int)y; // unboxing
+            Console.WriteLine($"x:{x}, y:{y}, z:{z}");
+        }
+    }
+}
---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CheckedAndUnchecked
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine($"Max integer value is: {int.MaxValue}");
+        //    int a = 2147483647;
+        //    int b = 2147483647;
+        //    //int c = checked(a + b); // throws an exception in case of an overflow
+        //    int c = unchecked(a + b);
+
+        //    const int num1 = 2147483647;
+        //    const int num2 = 2147483647;
+        //    int sum = unchecked(num1 + num2);
+        //    Console.WriteLine(sum);
+        //}
+    }
+}
