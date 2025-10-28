fd50296f376209172d01fe6462f1c85b165be9f4
codeme254 <otwomadennis365@gmail.com>
2025-10-28 06:19:33 +0300

    Day 8
    - Introduction to OOP
    - Classes and Objects

---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object (instance of the calculator class)
+            Calculator c = new Calculator();
+            // Accessing Calculator class member using an object
+            Console.WriteLine(c.Sum(5, 6));
+        }
+    }
+
+    // Defininig a class
+    class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
