ad64b32d6b6d7b47a7bb0cb0174042ffc58aae51
codeme254 <otwomadennis365@gmail.com>
2025-10-30 07:05:41 +0300

    Day 11
    - Destructors
    - Garbage Collectors

---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Destructors
+{
+    internal class DestructorDemo
+    {
+        public DestructorDemo()
+        {
+            Console.WriteLine("Constructor object created");
+        }
+
+        ~DestructorDemo()
+        {
+            Console.WriteLine("Object destroyed");
+        }
+        static void Main(string[] args)
+        {
+            DestructorDemo obj1 = new DestructorDemo();
+            DestructorDemo obj2 = new DestructorDemo();
+
+            // making obj1 eligible for garbage collection
+            obj1 = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
