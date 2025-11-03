559c0a140ae4c4a87c525ed1e9c41b931b695ee5
codeme254 <otwomadennis365@gmail.com>
2025-11-03 06:52:38 +0300

    Day 15
    - Abstract classes and methods
    - Interfaces

---
+++ Day15/Day15/Interfaces.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IMath
+    {
+        void Add(int x, int y);
+        void Sub(int x, int y);
+        void Mul(int x, int y);
+        void Div(int x, int y);
+    }
+
+    class Math : IMath
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public void Div(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+        public void Mod(int x, int y)
+        {
+            Console.WriteLine(x % y);
+        }
+    }
+    internal class Interfaces
+    {
+        static void Main(string[] args)
+        {
+            IMath m = new Math();
+            m.Mul(5, 3);
+            m.Sub(5, 3);
+            m.Add(5, 3);
+            m.Div(5, 3);
+            //m.mod(5, 3); // Error
+        }
+    }
+}
---
+++ Day15/Day15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AbstractClassesAndMethods
+{
+    abstract class AbstractClass
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+        public abstract void Mul(int x, int y);
+        public abstract void Div(int x, int y);
+    }
+
+    class AbstractChild : AbstractClass
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+    }
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    AbstractChild c = new AbstractChild();
+        //    c.Mul(5, 3);
+        //    c.Div(5, 3);
+        //    c.Add(5, 3);
+        //    c.Sub(5, 3);
+        //}
+    }
+}
