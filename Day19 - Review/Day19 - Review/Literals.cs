e7204554e53b96a9af0c3770a1acfe76e11090e7
codeme254 <otwomadennis365@gmail.com>
2025-11-07 06:49:46 +0300

    Day 19
    - Review data types in C#
    - Review Literals in C#

---
+++ Day19 - Review/Day19 - Review/Literals.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Literals
+{
+    internal class Literals
+    {
+        static void Main(string[] args)
+        {
+            // INTEGER LITERALS
+            // ================
+            // We can represent integer literals in terms of Decimal, Binary or Hexadecimal
+            // Binary - prefix with 0b
+            // Hexadecimal - prefix with 0X
+            // Decimal - no prefix required
+
+            // Decimal - base 10, no prefix required, values 0-9
+            int x = 209;
+            Console.WriteLine(x);
+            // Hexadecimal - digits 0 to 9 and characters a to f, prefix with 0X
+            int y = 0x99D;
+            Console.WriteLine(y);
+            // Binary - only 0 and 1 are allowed, prefix with 0b
+            int z = 0b011100111;
+            Console.WriteLine(z);
+
+            // FLOATING POINT LITERALS
+            // =======================
+            // decimal - suffix with m or M
+            // float - suffix with f or F
+            // double - no suffix needed but you can suffix with d or D
+            decimal d1 = 99.99m;
+            Console.WriteLine(d1);
+            float f1 = 88.88f;
+            Console.WriteLine(f1);
+            double d2 = 77.77d;
+            Console.WriteLine(d2);
+
+            // CHARACTER LITERALS
+            // ==================
+            // wrap within single quotes - 'A'
+            // every escape character can be specified as a character literal - '\n', '\t'
+            // we can represent character literals using unicode - '\uxxxx', where xxxx is the
+            //     4 hexadecimal numbers for example '\u0041'
+            char c1 = 'a';
+            Console.WriteLine(c1);
+            char c2 = '\n';
+            Console.WriteLine(c2);
+            char c3 = '\u0041';
+            Console.WriteLine(c3);
+
+            // STRING LITERALS
+            // ===============
+            // regular string literals - enclosed within double quotes
+            // verbatim string literals - starts with @ followed by double quotes.
+            // verbatim string literals can store escape sequence which is printed in the output
+            string s1 = "Hello \n World";
+            Console.WriteLine(s1);
+            string s2 = @"Hello \n World";
+            Console.WriteLine(s2);
+
+            // BOOLEAN LITERALS
+            // ================
+            // Store either true or false
+            bool b1 = true;
+            Console.WriteLine(b1);
+            bool b2 = false;
+            Console.WriteLine(b2);
+        }
+    }
+}
---
+++ Day19 - Review/Day19 - Review/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            // Byte - represents 8-bit unsigned integers (only positive values)
+            // 8-bit - meaning it can store 2^8 possible numbers - 256 numbers
+            // These numbers are 0 to 255
+            // ASCII characters map neatly to that 0 - 127 range thus we can use bytes to store ascii values
+            byte b1 = 69;
+            Console.WriteLine(b1);
+            Console.WriteLine($"Ascii equivalent of {b1} is {(char)b1}");
+            Console.WriteLine($"byte max value is {byte.MaxValue}");
+            Console.WriteLine($"byte min value is {byte.MinValue}");
+
+            // sbyte - 8-bit unsigned intenger (both positive and negative numbers0
+            // 8-bit - meaning it can store 2^8 possible numbers - 256 numbers
+            // Min value is -128 and max value is 127
+            sbyte b2 = 69;
+            Console.WriteLine(b2);
+            Console.WriteLine($"sbyte max value is {sbyte.MaxValue}");
+            Console.WriteLine($"sbyte min value is {sbyte.MinValue}");
+
+            // char - represents characters
+            char c1 = 'a';
+            Console.WriteLine(c1);
+            // we can also store unicode values in char
+            char c2 = '\u0061';
+            Console.WriteLine(c2);
+
+            // String - series of characters enclosed within double quotes
+            string username = "John";
+            Console.WriteLine($"Username is {username}");
+
+            // Boolean - only true or false values
+            bool x = true;
+            Console.WriteLine(x);
+
+            // INTEGER TYPES
+            // =============
+
+            // Integer types are divided into 3 as follows
+            // 16-bit signed: Int16 or short
+            // 32-bit signed: Int32 or int
+            // 64-bit signed: Int64 or long
+
+            // Unsigned versions of the above
+            // UInt16 or ushort
+            // UInt32 or uint
+            // UInt64 or ulong
+            //short a = 55;
+            //short na = -55;
+            //int b = 99;
+            //int nb = -99;
+            //long c = 200;
+            //long nc = -200;
+
+            //ushort ua = 200;
+            //uint ub = 900;
+            //ulong uc = 500;
+
+            Console.WriteLine($"short max value: {short.MaxValue}");
+            Console.WriteLine($"short min value: {short.MinValue}");
+            Console.WriteLine($"int min value: {int.MinValue}");
+            Console.WriteLine($"int max value: {int.MaxValue}");
+            Console.WriteLine($"long min value: {long.MinValue}");
+            Console.WriteLine($"long max value: {long.MaxValue}");
+
+            Console.WriteLine($"ushort min value: {ushort.MinValue}");
+            Console.WriteLine($"ushort max value: {ushort.MaxValue}");
+            Console.WriteLine($"uint min value: {uint.MinValue}");
+            Console.WriteLine($"uint max value: {uint.MaxValue}");
+            Console.WriteLine($"ulong min value: {ulong.MinValue}");
+            Console.WriteLine($"ulong max value: {ulong.MaxValue}");
+
+            // DECIMAL TYPES
+            // =============
+            // Store numbers with decimal points
+            // float/Single: suffix number with f
+            // decimal/Decimal: suffix number with m
+            // double/Double: no suffixes needed
+            float f1 = 33.5f;
+            Console.WriteLine(f1);
+            decimal d1 = 33.6m;
+            Console.WriteLine(d1);
+            double d2 = 99.9;
+            Console.WriteLine(d2);
+
+            // GETTING THE SIZE OF BUILT-IN DATA TYPES
+            // =======================================
+            // use the sizeof() operator which returns the size of a data type in bytes
+            Console.WriteLine($"Size of int is {sizeof(int)} bytes");
+            Console.WriteLine($"Size of decimal is {sizeof(decimal)} bytes");
+            Console.WriteLine($"Size of char is {sizeof(char)} bytes");
+            Console.WriteLine($"Size of double is {sizeof(double)} bytes");
+
+            // ASCII TABLE
+            //for (int i = 0; i <= 127; i++)
+            //{
+            //    Console.WriteLine($"ASCII equivalent of {i} is {(char)i}");
+            //}
+        }
+    }
+}
