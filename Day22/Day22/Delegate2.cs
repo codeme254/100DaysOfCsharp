ea4763caac8ed2168674ddfcf396af200fabf0cb
codeme254 <otwomadennis365@gmail.com>
2025-11-10 06:54:23 +0300

    Day 22
    - Delegates

---
+++ Day22/Day22/Delegate2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Delegates
+    {
+        // Delegate
+        public delegate void GreetDelegate(string name);
+
+        // SayHello method that matches the signature of GreetDelegate delegate
+        void SayHello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine($"How do you do {name}");
+        }
+
+        // SayGoodbye method that matches the signature of GreetDelegate delegate
+        void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Goodbye {name}");
+        }
+        static void Main(string[] args)
+        {
+            Delegates d = new Delegates();
+            GreetDelegate greet;
+            greet = d.SayHello;
+            greet("Dennis");
+
+            GreetDelegate gd = new GreetDelegate(d.SayHello);
+            gd("Dennis");
+            gd = new GreetDelegate(d.SayGoodbye);
+            gd("Dennis");
+        }
+    }
+}
---
+++ Day22/Day22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        // Delegate
+        public delegate void GreetDelegate(string name);
+
+        // SayHello method that matches the signature of GreetDelegate delegate
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine($"How do you do {name}");
+        }
+
+        // SayGoodbye method that matches the signature of GreetDelegate delegate
+        static void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Goodbye {name}");
+        }
+        static void Main1(string[] args)
+        {
+            GreetDelegate greet;
+            greet = SayHello;
+            greet("Dennis");
+
+            greet = SayGoodbye;
+            greet("Dennis");
+        }
+    }
+}
