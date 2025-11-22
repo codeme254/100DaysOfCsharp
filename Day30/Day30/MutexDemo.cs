f52da3437c54c75ccb0ce9e4d7e7e92d466b62a8
codeme254 <otwomadennis365@gmail.com>
2025-11-22 18:08:59 +0300

    Day 30
    - Mutex class

---
+++ Day30/Day30/MutexDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexExample
+{
+    internal class Program
+    {
+        private static Mutex mutex = new Mutex();
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread threadObject = new Thread(MutexDemo)
+                {
+                    Name = $"Thread {i}"
+                };
+                threadObject.Start();
+            }
+        }
+        static void MutexDemo()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} wants to enter criticla section for processing");
+            try
+            {
+                // Blocks the current thread until the current waitONe method receives a signal
+
+                // wait until it is safer to enter
+                mutex.WaitOne();
+                Console.WriteLine($"Success: {Thread.CurrentThread.Name} is processing now.");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Exit: {Thread.CurrentThread.Name} has completed its task.");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
---
+++ Day30/Day30/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexDemo
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            using(Mutex mutex = new Mutex(false, "MutexDemo"))
+            {
+                // Check of another external thread is running
+                if (!mutex.WaitOne(500,false))
+                {
+                    Console.WriteLine("An instance of the application is already running");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("Application is running...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
