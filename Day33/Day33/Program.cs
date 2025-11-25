787d8824fe02ba60ca3b7dc01179335d6b830845
codeme254 <otwomadennis365@gmail.com>
2025-11-25 06:55:17 +0300

    Day 33
    - Non-Generic Hashtable Collection
    - Non-Generic Stack Collection

---
+++ Day33/Day33/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace HashtableCollection
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Hashtable employeeDetails = new Hashtable();
+            employeeDetails.Add("Id", 1000);
+            employeeDetails.Add("FirstName", "John");
+            employeeDetails.Add("LastName", "Doe");
+            employeeDetails.Add("Age", 35);
+            employeeDetails.Add("EmailAddress", "john@company.com");
+
+            // Adding elements using the indexer
+            employeeDetails["Salary"] = 30000;
+            employeeDetails["Gender"] = "Male";
+
+            // Updating values using indexer
+            employeeDetails["FirstName"] = "Jack"; // Updates from John to Jack
+            employeeDetails["LastName"] = "Smith"; // Updates from Doe to Smith
+
+            // Adding elements using collection initializer syntax
+            Hashtable employee = new Hashtable()
+            {
+                {"Id", 1000},
+                {"FirstName", "John"},
+                {"LastName", "Doe"},
+                {"Age", 35},
+                {"EmailAddress", "john@company.com"}
+            };
+
+            Console.WriteLine(employeeDetails["FirstName"]);
+            Console.WriteLine(employeeDetails["LastName"]);
+
+            if (employeeDetails.ContainsKey("Age"))
+            {
+                employeeDetails.Remove("Age");
+            }
+
+            foreach(DictionaryEntry entry in employeeDetails)
+            {
+                Console.WriteLine($"{entry.Key} - {entry.Value}");
+            }
+
+            // Checking for existence of keys
+            Console.WriteLine(employeeDetails.Contains("Id"));// True
+            Console.WriteLine(employeeDetails.Contains("Location")); // False
+
+            // Checking for existence of values
+            Console.WriteLine(employeeDetails.ContainsValue("John")); // True
+            Console.WriteLine(employeeDetails.ContainsValue("Sales and Marketing")); // False
+        }
+    }
+}
---
+++ Day33/Day33/StackCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace StackCollection
+{
+    internal class StackCollection
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push("John");
+            stack.Push(10);
+            stack.Push(3.14f);
+            stack.Push("Hello");
+            stack.Push("A");
+            stack.Push(true);
+            stack.Push(false);
+
+            Console.WriteLine(stack.Peek()); // False
+
+            stack.Pop();
+            stack.Pop();
+
+            // You can also remove all items using the Clear method
+            //stack.Clear();
+
+            Console.WriteLine(stack.Contains("Hello")); // True
+            Console.WriteLine(stack.Contains("World")); // False
+
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
