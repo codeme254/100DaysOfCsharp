b5e6e773060fb8ca42bc418b89c6a3a7d01024c9
codeme254 <otwomadennis365@gmail.com>
2025-11-27 07:10:13 +0300

    Day 34
    - Non-Generic Queue Collection
    - Non-Generic Stack Collection
    - Advantages and Disadvantages of Non-Generic Collections

---
+++ Day34/Day34/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueCollection
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue(101);
+            q.Enqueue("Hello");
+            q.Enqueue(3.142f);
+            q.Enqueue(false);
+            q.Enqueue('A');
+
+            //q.Dequeue();
+            //q.Dequeue();
+
+            Console.WriteLine($"First element: {q.Dequeue()}"); // 101
+            Console.WriteLine($"First element: {q.Peek()}"); // "Hello"
+
+            Console.WriteLine(q.Contains("Hello")); // True
+            Console.WriteLine(q.Contains("World")); // False
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Day34/Day34/SortedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace SortedListCollection
+{
+    internal class SortedListCollection
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+            sl.Add(1, "One");
+            sl.Add(2, "Two");
+            sl.Add(3, "Three");
+            sl.Add(50, 5);
+            sl.Add(100, null);
+
+            //Console.WriteLine($"Key 100, value {sl[100]}"); // null
+            //Console.WriteLine($"Key 50, value {sl[50]}"); // 5
+            //Console.WriteLine($"Key 1, value {sl[1]}");  // One
+
+            //Console.WriteLine(sl.GetByIndex(0)); // One
+            //Console.WriteLine(sl.GetByIndex(1)); // Two
+            //Console.WriteLine(sl.GetByIndex(3)); // 5
+
+            //for (int i = 0; i < sl.Count; i++)
+            //{
+            //    Console.WriteLine("---");
+            //    Console.WriteLine($"Key: {sl.GetKey(i)}");
+            //    Console.WriteLine($"Value: {sl.GetByIndex(i)}");
+            //    Console.WriteLine("---");
+            //}
+
+            //foreach(DictionaryEntry item in sl)
+            //{
+            //    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            //}
+
+            SortedList sortedList = new SortedList()
+            {
+                {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}
+            };
+            //sortedList.Remove("two");
+            //sortedList.RemoveAt(2);
+            Console.WriteLine(sortedList.Contains("one")); // True
+            Console.WriteLine(sortedList.ContainsKey("two")); // True
+            Console.WriteLine(sortedList.ContainsKey("hello")); // False
+            Console.WriteLine(sortedList.ContainsValue(500)); // False
+            Console.WriteLine(sortedList.ContainsValue(4)); // True
+            Console.WriteLine(sortedList.ContainsValue("hello world")); // False
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+    }
+}
