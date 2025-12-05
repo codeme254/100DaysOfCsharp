31e21bd3ebefb3510e047bb66228a337213f1076
codeme254 <otwomadennis365@gmail.com>
2025-12-05 07:37:31 +0300

    Day 38
    - Generic SortedSet Collection
    - Generic SortedDictionary Collection

---
+++ Day38/Day38/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSetCollection
+{
+    internal class Program
+    {
+        static void Main1(string[]args)
+        {
+            //SortedSet<int> st = new SortedSet<int>();
+            //st.Add(10);
+            //st.Add(5);
+            //st.Add(60);
+            //st.Add(70);
+            //st.Add(25);
+            //st.Add(60);
+
+            ////st.Remove(10);
+            ////st.Remove(5);
+
+            ////st.RemoveWhere(num => num < 20);
+
+            ////st.Clear();
+            //Console.WriteLine(st.Contains(10)); // True
+            //Console.WriteLine(st.Contains(100)); // False
+
+            //foreach (int i in st)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            SortedSet<int> set1 = new SortedSet<int>()
+            {
+                1, 2, 3, 4
+            };
+            SortedSet<int> set2 = new SortedSet<int>()
+            {
+                3, 4, 5, 6
+            };
+
+            //set1.UnionWith(set2);
+            //set1.IntersectWith(set2);
+            //set1.ExceptWith(set2);
+            set1.SymmetricExceptWith(set2);
+
+            foreach (int i in set1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Day38/Day38/SortedDictionaryCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SortedDictionaryCollection
+{
+    internal class SortedDictionaryCollection
+    {
+        static void Main1(string[] args)
+        {
+            SortedDictionary<int, string> sd = new SortedDictionary<int, string>();
+            sd.Add(1, "One");
+            sd.Add(2, "Two");
+            sd.Add(10, "Ten");
+            sd.Add(3, "Three");
+            sd.Add(5, "Five");
+            sd.Add(7, "Seven");
+
+            sd[4] = "Four";
+            sd[6] = "Six";
+            sd[8] = "Eight";
+            sd[9] = "Nine";
+
+            sd[8] = "New Eight";
+            sd[5] = "New Five";
+
+            //Console.WriteLine(sd[2]); // Two
+            //Console.WriteLine(sd[10]); // Ten
+            //Console.WriteLine(sd[7]); // Seven
+
+            //sd.Remove(10);
+            //sd.Remove(5);
+            //sd.Remove(7);
+
+            //sd.Clear();
+
+            //Console.WriteLine(sd.ContainsKey(10)); // True
+            //Console.WriteLine(sd.ContainsKey(1000)); // False
+
+            //Console.WriteLine(sd.ContainsValue("Three")); // True
+            //Console.WriteLine(sd.ContainsValue("Nine")); // False
+
+            bool tenExists = sd.TryGetValue(10, out string ten);
+            if (tenExists)
+            {
+                Console.WriteLine(ten);
+            }
+            else
+            {
+                Console.WriteLine("Value does not exist");
+            }
+
+            bool twentyExists = sd.TryGetValue(20, out string twenty);
+            if (twentyExists)
+            {
+                Console.WriteLine(twenty);
+            }
+            else
+            {
+                Console.WriteLine("Twenty does not exist");
+            }
+
+                foreach (KeyValuePair<int, string> kvp in sd)
+                {
+                    Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+
+
+            Console.WriteLine("===KEYS===");
+            foreach(int key in sd.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            Console.WriteLine("===VALUES===");
+            foreach(string value in sd.Values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
