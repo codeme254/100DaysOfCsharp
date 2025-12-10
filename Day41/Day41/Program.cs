3046838e123f9aa4ab1b2e1e9d274309b6392d54
codeme254 <otwomadennis365@gmail.com>
2025-12-10 07:26:44 +0300

    Day 41
    - async and await

---
+++ Day41/Day41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started");
+            SomeMethod();
+            Console.WriteLine("Main method ended");
+            Console.ReadKey();
+        }
+
+        async static void SomeMethod()
+        {
+            Console.WriteLine("SomeMethod started");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("SomeMethod ended");
+        }
+    }
+}
