757cb4a34f2e1e33cb325164b2ba99ac2f2c7045
codeme254 <otwomadennis365@gmail.com>
2026-01-28 06:30:30 +0300

    Day 53
    - IEnumerable and IQueryable
    - Extension methods

---
+++ Day53/Day53/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class ExtensionMethods
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hello World, C# is fun";
+            int wordCount = sentence.GetWordCount();
+            Console.WriteLine(wordCount); // 5
+        }
+    }
+    public static class ExtensionHelper
+    {
+        public static int GetWordCount(this string str)
+        {
+            if (!String.IsNullOrEmpty(str))
+            {
+                return str.Split(' ').Length;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Day53/Day53/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerableAndIQueryable
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            List<int> list = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            IEnumerable<int> evenNumbers = from obj in list
+                              where obj % 2 == 0
+                              select obj;
+
+            foreach(int num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            IQueryable<int> oddNumbers =  list.AsQueryable()
+                .Where(num =>  num % 2 != 0);
+
+            foreach(int num in oddNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
