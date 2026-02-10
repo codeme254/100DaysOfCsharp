e688a9a44172cbef2f09e77533ade8fb98f66624
codeme254 <otwomadennis365@gmail.com>
2026-02-10 08:10:42 +0300

    Day 59
    - Group By method

---
+++ Day59/Day59/GroupBy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudentData;
+
+namespace GroupByMethod
+{
+    class StudentGroupingKey
+    {
+        public string Branch { get; set; }
+        public string Gender {  get; set; }
+    }
+    internal class GroupByMethod
+    {
+        static void Main(string[] args)
+        {
+            // Method Syntax
+            IEnumerable<IGrouping<string, Student>> studentGrouping = Student.GetStudents().GroupBy(s => s.Branch);
+            foreach(IGrouping<string, Student> group in studentGrouping)
+            {
+                Console.WriteLine($"{group.Key} : {group.Count()}");
+                foreach(Student student in group)
+                {
+                    Console.WriteLine($" {student.Name} - {student.Gender} - {student.Branch}");
+                }
+            }
+
+            // Query Syntax
+            studentGrouping = (from std in Student.GetStudents()
+                               group std by std.Branch);
+            foreach (IGrouping<string, Student> group in studentGrouping)
+            {
+                Console.WriteLine($"{group.Key} : {group.Count()}");
+                foreach (Student student in group)
+                {
+                    Console.WriteLine($" {student.Name} - {student.Gender} - {student.Branch}");
+                }
+            }
+
+            Console.WriteLine("==================");
+            // Grouping by Branch and Gender
+            IEnumerable<IGrouping<StudentGroupingKey, Student>> groupByBranchAndGender =
+                Student.GetStudents()
+                       .GroupBy(s => new StudentGroupingKey
+                       {
+                           Branch = s.Branch,
+                           Gender = s.Gender
+                       });
+
+            foreach (IGrouping<StudentGroupingKey, Student> group in groupByBranchAndGender)
+            {
+                Console.WriteLine($"{group.Key.Branch} - {group.Key.Gender} : {group.Count()}");
+
+                foreach (Student student in group)
+                {
+                    Console.WriteLine($" {student.Name} - {student.Gender} - {student.Branch}");
+                }
+            }
+
+        }
+    }
+}
